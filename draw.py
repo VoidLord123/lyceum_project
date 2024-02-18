bde105055264fe796ed76e991c7a382b8cb095ac
D4rkSn0w <yaroslav.galyushin@gmail.com>
2024-02-18 22:55:36 +0300

    made project

--- draw.py
+++ draw.py
@@ -2,11 +2,14 @@ import requests
 from io import BytesIO
 
 
-def draw(ll, spn, mode='map'):
+def draw(ll, spn, pt=[], mode='map', size="100,100"):
     map_params = {
         "ll": ",".join(map(str, ll)),
         "spn": ",".join(map(str, spn)),
-        "l": mode}
+        "l": mode,
+        "size": size,
+        "pt": pt
+    }
     map_api_server = "http://static-maps.yandex.ru/1.x/"
     response = requests.get(map_api_server, params=map_params)
     if not response:
--- geocoder.py
+++ geocoder.py
@@ -1,3 +1,4 @@
+import json
 from pprint import pprint
 
 import requests
@@ -19,9 +20,45 @@ def get_info(object_name):
         return member
     except Exception:
         return None
+    return None
 
 
-def get_object_coord(object_name):
-    obj = get_info(object_name)
-    if obj:
-        return obj["Point"]['pos']
+def get_object_coord(member):
+    try:
+        return member["Point"]["pos"].split()
+    except KeyError:
+        return ''
+
+
+def get_object_address(member):
+    try:
+        return member["metaDataProperty"]['GeocoderMetaData']['Address']['formatted']
+    except KeyError:
+        return ''
+
+
+def get_object_postcode(member):
+    try:
+        return member["metaDataProperty"]['GeocoderMetaData']['Address']['postal_code']
+    except KeyError:
+        return ''
+
+
+def get_organisation_properties(address_ll):
+    search_params = {
+        "apikey": 'dda3ddba-c9ea-4ead-9010-f43fbc15c6e3',
+        "lang": "ru_RU",
+        "text": get_object_address(get_info(address_ll)),
+        "type": 'biz',
+        "ll": address_ll
+    }
+    search_api_server = "https://search-maps.yandex.ru/v1/"
+    response = requests.get(search_api_server, params=search_params)
+    if not response:
+        pass
+    json_response = response.json()["features"]
+    organization = json_response[0]
+    company_data = organization["properties"]["CompanyMetaData"]
+    address = company_data["address"]
+    coords = organization['geometry']['coordinates']
+    return address, coords
